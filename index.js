b9f1786cf2996e96220ecc14f16d278b8984bbf6
shikubro90 <shikubro90@gmail.com>
2023-01-31 04:58:49 +0600

    update

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const app = express();
 port = 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
+
+//openai require
 const { Configuration, OpenAIApi } = require("openai");
+
 const configuration = new Configuration({
   organization: "org-OQX9rFaJmU3LrNAqarm8RpVN",
-  apiKey: "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",
+  apiKey: "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",
 });
-// const response = await openai.listEngines();
 
 const openai = new OpenAIApi(configuration);
 
 app.post("/", async (req, res) => {
-  const { message } = req.body;
-      const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${message}`,
-      max_tokens: 100   ,
-      temperature: 0.5,
-    });
+  const { message, currentModel } = req.body;
+  const response = await openai.createCompletion({
+    model: `${currentModel}`, //"text-davinci-003",
+    prompt: `${message}`,
+    max_tokens: 100,
+    temperature: 0.5,
+  });
 
   res.status(200).json({
-    message: response.data.choices[0].text
+    message: response.data.choices[0].text,
   });
 });
 
-app.listen(port, (req, res) => {
+app.get("/models", async (req, res) => {
+  const response = await openai.listEngines()
+  console.log(response.data)
+  res.json({
+    models : response.data
+  })
+});
+
+app.listen(port, () => {
   console.log(`Example app listening http://localhost:${port}`);
 });
